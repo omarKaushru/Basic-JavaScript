d481f462fa12f5e27e4801378c34f69a112c7c96
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-08-31 12:16:04 +0600

    JS output, syntax, input are done

---
+++ 1.2 Connect JS file to HTML/JS/script.js
@@ -0,0 +1,6 @@
+document.write("JS file is connectd");
+var fullName; //variable decleration
+//prompt takes input from the user. one can also sgow a message during input session by
+// just using cotation inside prompt.
+fullName = prompt("Enter your full name"); 
+document.write(fullName);
