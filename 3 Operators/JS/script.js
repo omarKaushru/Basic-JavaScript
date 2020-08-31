466a6bb14e0a687a6cc748f54a7417e32e71ee47
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-08-31 15:03:19 +0600

    JS arithmetic operator

---
+++ 3 Operators/JS/script.js
@@ -0,0 +1,27 @@
+// Arithmetic Operators
+var number1, number2, result;
+number1 = 23;
+number2 =5;
+result = number1 + number2;
+document.writeln (result);
+result = number1 - number2;
+document.writeln (result);
+result = number1 * number2;
+document.writeln (result);
+result = number1 / number2;
+document.writeln (result);
+result = number1 % number2;
+document.writeln (result);
+result = number2 ** 5; //it means numbe2 to the power 5
+document.writeln (result);
+
+number2++;
+document.writeln (number2);
+number2--;
+document.writeln (number2);
+//post increment
+result = number1++; // it store first the value of number after than increment number1
+document.writeln (result);
+//pre increment
+result = ++number1; // it first increments then store it into result.
+document.writeln (result);
