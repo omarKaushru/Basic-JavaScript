59f3b735cda82f6edcdbf4260f8b63c763811dce
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-09-02 10:04:12 +0600

    Template Literals

---
+++ 5 Template Literals/JS/script.js
@@ -0,0 +1,21 @@
+// Template Literals (ES6) // helps to deal with multiple line of string output
+// Backtick ``
+console.log(`First Line
+Second line.
+Third Line`); //ouput comes in separates lines without using "\n", backtic(``) let us to do it.
+//within a variable.  
+// let a = `zaq
+// xsw
+// cde`;
+// console.log(a);
+let name = "Rahim";
+let age = "38";
+let dob = "21 June, 1983";
+console.log(`His Name is: ${name}
+His age is: ${age}
+His DOB is: ${dob}`);
+
+
+let a = 34;
+let b = 67;
+console.log(`${a} + ${b} = ${ a + b }`);
