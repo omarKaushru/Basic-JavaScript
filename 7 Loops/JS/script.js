64132a0553e37aa4fc34d9704045c2692abb8cd5
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-09-02 14:32:35 +0600

    loop

---
+++ 7 Loops/JS/script.js
@@ -0,0 +1,58 @@
+/////////// Loops
+
+/////////////////////////// While Loop ///////////////
+
+/*
+
+let i = 1;
+
+while(i<=9)
+{
+    console.log("Value of i: " +i);
+    i++;
+}
+
+let i = 10;
+
+do{
+    console.log("Value of i: " +i);
+    i++;
+}while(i<=20);
+
+*/
+
+/////////////////////////////////// for loop  ////////////////////////////
+/*
+for (var j = 1; j<=5 ; j++) {
+    console.log(j);
+}
+
+for (var k = 5; k >=1; k-- ) {
+    console.log(k);
+}
+*/
+
+/////////////////////////Break and Continue//////////////////////////////////
+
+/* 
+// break 
+
+for(var i=1; i<=5; i++ ) {
+    if ( i == 3) {
+        break;
+    }
+    console.log(i);
+}
+
+
+
+// continue
+
+for (var i =1; i <= 10; i++){
+    if(i % 2 == 1) {
+        continue;
+    }
+    console.log(i);
+}
+
+*/
