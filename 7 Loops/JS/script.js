211bcc135859924e5b201efa2e38ae956957b1f0
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-09-03 09:39:17 +0600

    Loops

--- 7 Loops/JS/script.js
+++ 7 Loops/JS/script.js
@@ -56,3 +56,89 @@ for (var i =1; i <= 10; i++){
 }
 
 */
+
+//////////////////// String and Array Iteration ///////////////
+// String and array
+/*
+let name = "I am learning Javascript!";
+let food = ["Cake", "Chocolate", "Ice Cream"];
+let len = food.length;
+
+for(var i = 0; i < len; i++) {
+    console.log(`Index : ${i}`);
+    console.log(food[i]); 
+}
+
+//reverse
+
+for(var i = len-1; i =>0; i--) {
+    console.log(food[i]); 
+}
+
+
+len = name.length;
+for(var i =0; i<len; i++)
+{
+    console.log(`Index : ${i}`);
+    console.log(name[i]); 
+}
+
+//reverse
+
+for(var i = len-1; i =>0; i--) {
+    console.log(name[i]); 
+}
+*/
+
+/////////////////////////////// For-in and For-of ////////////////////////////////////
+
+//for-in works with string, array, object
+// for of 
+// string, array 
+
+let name = "I am learning Javascript!";
+let food = ["Cake", "Chocolate", "Ice Cream"];
+let person = {
+    Name: "Md Omar Kaushru",
+    Profession: "Software Enginner",
+    Company : "Own",
+    Age: 28
+};
+//it takes the indexes of a varible and keeps them in a value for example "x" 
+/* for (var x in name) {
+console.log(name[x]);
+// document.writeln(name[x]);
+} */
+/*
+//it takes the values of a varible and keeps them in a value for example "x" 
+for (var x of name){
+    console.log(x);
+}
+*/
+// objects don't have index
+
+/*
+for (var x in person){
+    console.log(`Property: ${x}
+    Value: ${person[x]}`)
+} */
+
+// For-of won't work for objects
+
+// Exercise and Solution
+
+var n = prompt("Enter the value of N");
+var sum=0;
+var series = ""; //creating an empty string 
+for(var i=1; i<=n; i++)
+{
+    sum = sum + i**2;
+    series += (i ** 2).toString();
+    if (i == n) 
+    { 
+        continue; 
+    }
+    series += " + ";
+}
+// for n =5 it will have such output: 1 + 4 + 9 + 16 + 25 = 55
+console.log(`${series} = ${sum}`);
