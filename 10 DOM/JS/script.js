f96edbc55f334632a3afb5dcd54c91802717e0be
Md. Omar Kaushru <omarkaushru@gmail.com>
2020-10-03 09:26:51 +0600

    DOM Traversing

--- 10 DOM/JS/script.js
+++ 10 DOM/JS/script.js
@@ -90,44 +90,80 @@
                         // /////DOM Selector (Multiple Elements)
 // Multiselector 
 // document.getElementsByClassName()
-let list = document.getElementsByClassName('sample-class');
-list[0].style.background = 'red';
-list[0].style.color = 'white';
-list[0].style.padding = '10px';
-list[0].textContent = 'Umar Ibn Hasan';
-//console.log(list[0]);
+// let list = document.getElementsByClassName('sample-class');
+// list[0].style.background = 'green';
+// list[0].style.color = 'white';
+// list[0].style.padding = '10px';
+// list[0].textContent = 'Umar Ibn Hasan';
+// //console.log(list[0]);
 
-// document.getElementsByTagName()
-list = document.getElementsByTagName('li');
-list = document.querySelector('ol').getElementsByTagName('li');
+// // document.getElementsByTagName()
+// list = document.getElementsByTagName('li');
+// list = document.querySelector('ol').getElementsByTagName('li');
 
-let lis = Array.from(list);
-lis.forEach(function(item){
-    //console.log(item);
-});
+// let lis = Array.from(list);
+// lis.forEach(function(item){
+//     //console.log(item);
+// });
 
-// document.querySelectorAll()
-// id -> #
-// classname -> .
-// tagname -> nothing
+// // document.querySelectorAll()
+// // id -> #
+// // classname -> .
+// // tagname -> nothing
 
-list = document.querySelectorAll('ol li');
-list.forEach(function(item){
-    //console.log(item);
-});
+// list = document.querySelectorAll('ol li');
+// list.forEach(function(item){
+//     //console.log(item);
+// });
 
-let liOdd = document.querySelectorAll('li:nth-child(odd)');
-let liEve = document.querySelectorAll('li:nth-child(even)');
+// let liOdd = document.querySelectorAll('li:nth-child(odd)');
+// let liEve = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function(item){
-    item.style.background = 'grey';
-    item.style.color = 'white';
-});
+// liOdd.forEach(function(item){
+//     item.style.background = 'grey';
+//     item.style.color = 'white';
+// });
 
-liEve.forEach(function(item){
-    item.style.background = 'red';
-    item.style.color = 'white';
-});
+// liEve.forEach(function(item){
+//     item.style.background = 'red';
+//     item.style.color = 'white';
+// });
+// 
+// console.log(list);
 
 
-console.log(list);
+////////////// Traversing the DOM
+
+let val;
+
+let list = document.querySelector('ul');
+let listItem = document.querySelector('ul li');
+
+val = list;
+val = listItem;
+
+//get the child node
+val = list;
+val = list.childNodes;
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[1];
+val = list.childNodes[2];
+val = list.childNodes[1].nodeName;
+val = list.childNodes[0].nodeType;
+///output of childnodes
+// 1 Element
+// 2 Attribute
+// 3 Text Node
+// 8 Comment
+// 9 Document Itself
+// 10 Doctype
+val = list.childNodes;
+
+val = list.children;
+val = list.children[1];
+list.children[0].textContent = "Hello";
+val = list.children[1].children[0];
+val = list.children[1].children[0].href;
+
+console.log(val);
